3b56a5e685bfbaf15131ac7d906496b12be9d43e
Diana Sica <diana.sica29@gmail.com>
2021-01-01 22:33:20 +0100

    styled the notification with a large icon

--- app/src/main/java/com/udacity/utils/NotificationUtils.kt
+++ app/src/main/java/com/udacity/utils/NotificationUtils.kt
@@ -5,11 +5,17 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.udacity.DetailActivity
 import com.udacity.R
+
 
 const val NOTIFICATION_ID = 0
 
@@ -35,7 +41,7 @@ fun sendNotification(fileName: String, downloadStatus: String, context: Context)
     var notificationDescription = ""
     when(fileName) {
         context.getString(R.string.custom_file) -> {
-            when(downloadStatus) {
+            when (downloadStatus) {
                 "Success" -> notificationDescription = context.getString(R.string.custom_file_notification_description, fileName, "complete")
                 "Fail" -> notificationDescription = context.getString(R.string.custom_file_notification_description, fileName, "failed")
             }
@@ -43,9 +49,23 @@ fun sendNotification(fileName: String, downloadStatus: String, context: Context)
         when(downloadStatus) {
             "Success" -> notificationDescription = context.getString(R.string.notification_description, fileName, "complete")
             "Fail" -> notificationDescription = context.getString(R.string.notification_description, fileName, "failed")
-        }
+            }
         }
     }
+
+    val successLargeIcon = ContextCompat.getDrawable(context, R.drawable.ic_baseline_thumb_up_24)
+    val failureLargeIcon = ContextCompat.getDrawable(context, R.drawable.ic_baseline_thumb_down_24)
+    val statusImage = when(downloadStatus) {
+        "Success" -> drawableToBitmap(
+                successLargeIcon!!
+        )
+        else -> drawableToBitmap(
+                failureLargeIcon!!
+        )
+    }
+    val bigPictureStyle = NotificationCompat.BigPictureStyle()
+        .bigPicture(statusImage)
+        .bigLargeIcon(null)
 
     val builder = NotificationCompat.Builder(
             context,
@@ -56,6 +76,8 @@ fun sendNotification(fileName: String, downloadStatus: String, context: Context)
             .setContentText(notificationDescription)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
+            .setStyle(bigPictureStyle)
+            .setLargeIcon(statusImage)
             .addAction(
                     R.drawable.ic_assistant_black_24dp,
                     context.getString(R.string.check_status_button),
@@ -84,4 +106,28 @@ private fun createChannel(context: Context, channelId: String, channelName: Stri
         notificationManager.createNotificationChannel(notificationChannel)
 
     }
+}
+
+fun getBitmapFromVectorDrawable(context: Context?, drawableId: Int): Bitmap? {
+    val drawable = ContextCompat.getDrawable(context!!, drawableId)
+    val bitmap = Bitmap.createBitmap(drawable!!.intrinsicWidth,
+            drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
+}
+
+fun drawableToBitmap(drawable: Drawable): Bitmap? {
+    if (drawable is BitmapDrawable) {
+        return drawable.bitmap
+    }
+    val bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth,
+            drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888)
+    val canvas = Canvas(bitmap)
+    drawable.setBounds(0, 0, canvas.width, canvas.height)
+    drawable.draw(canvas)
+    return bitmap
 }
